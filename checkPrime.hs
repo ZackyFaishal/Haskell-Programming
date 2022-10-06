3866073426fc71bfbc652fde3f95945a990d3140
ZackyFaishal <zacky.faishal.tif421@polban.ac.id>
2022-10-06 22:23:27 +0700

    add checkPrime.hs

---
+++ checkPrime.hs
@@ -0,0 +1,13 @@
+cekPrime :: Integer -> Bool
+cekPrime 1 = False
+cekPrime 2 = True
+cekPrime n = cekprime n n
+
+cekprime :: Integer-> Integer -> Bool
+cekprime a b = 
+    if (b < 2 )
+        then True 
+    else 
+        if (mod a b == 0 && a /= b) 
+            then False 
+        else cekprime a (b-1)
